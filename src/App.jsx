72e4fd1cba69ce5340324d2f33f031eddc7ec765
Madhu@009 <717822y127@kce.ac.in>
2025-06-26 11:16:03 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+import './App.css'
+import io from 'socket.io-client'
+import { useEffect, useState } from 'react'
+
+const socket = io('http://localhost:3000')
+function App() {
+
+  const [input, setInput] = useState('');
+  const [msg, setMessage] = useState([]);
+  const [username, setUsername] = useState('');
+
+  useEffect(() => {
+
+    const name = prompt("Enter you name: ");
+    if(name)
+    setUsername(name);
+
+    socket.on('chatMessage', (msg) => {
+      setMessage((prev) => [...prev, msg]);
+    })
+    return () => socket.off('chatMessage');
+  }, []);
+
+  const send = () => {
+    if (input.trim() !== '') {
+      const msgData = { user: username, text: input }
+      socket.emit('chatMessage', msgData);
+      setInput('');
+    }
+  }
+  return (
+    <div className="chat-container">
+
+      <h2>Chat App</h2>
+      <ul>
+        <li 
+  key={input}
+  className={`message-bubble ${msg.user === username ? "My-message" : "Another-user"}`} >
+  <strong>{msg.user}:</strong>{msg.text}
+</li>
+        </ul>
+      <div className="input-section">
+        <input
+          type="text"
+          value={input}
+          placeholder='Your message...'
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button onClick={send}>Send</button>
+      </div>
+    </div>
+  )
+}
+
+export default App
