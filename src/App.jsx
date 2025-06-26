df8b06ff744d56001523a697aec1d727b0e5dd03
Madhu@009 <717822y127@kce.ac.in>
2025-06-26 12:09:19 +0530

    app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import io from 'socket.io-client'
 import { useEffect, useState } from 'react'
 
-const socket = io('http://localhost:3000')
+const socket = io('https://chat-application-rdqn.onrender.com')
 function App() {
 
   const [input, setInput] = useState('');
